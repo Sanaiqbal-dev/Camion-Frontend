693e35be37bf9b205df90d506684fff7ed397905
SanaIqbal <95401511+Sanaiqbal-dev@users.noreply.github.com>
2024-04-26 11:15:38 +0500

    tracking screen fixes

--- src/components/Carrier/Tracking.tsx
+++ src/components/Carrier/Tracking.tsx
@@ -69,9 +69,11 @@ const Tracking = () => {
         defaultZoom={13}
         mapId="9b0a2c44ded1af0e"
         style={{
-          height: "100%",
-          zIndex: "0",
-          position: "relative",
+          width:"calc(100vw - 360px)",
+          height: "calc(100vh - 90px)",
+          marginTop:"15px",
+          // zIndex: "0",
+          position: "absolute",
         }}
       >
         {markers.map((item) => (
