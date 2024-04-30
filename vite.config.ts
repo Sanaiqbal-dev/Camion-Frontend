3db2a7d201380dbcf2aa00b4186559b217d041bf
SanaIqbal <95401511+Sanaiqbal-dev@users.noreply.github.com>
2024-04-30 17:56:07 +0500

    added path alias in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 // import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
@@ -9,4 +10,11 @@ export default defineConfig({
       include: "**/*.tsx",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
 });
