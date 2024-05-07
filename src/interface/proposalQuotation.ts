2407d58dc12826d0c126d958744903bce4c19c10
Rohaan <rohaan.hussain@codeninjaconsulting.com>
2024-05-07 13:42:16 +0500

    get quotations and allow user to accept or reject

---
+++ src/interface/proposalQuotation.ts
@@ -0,0 +1,11 @@
+export interface IProposalQuotationUpdateBody {
+  amount: string;
+  delieveryDate: string;
+  otherDetails: string;
+  proposalQuotationId: number;
+  proposalQuotationStatusId: number;
+  fileName: string;
+  filePath: string;
+  purposalId: number;
+  userId: string;
+}
